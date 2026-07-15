0a1fb08dead39e341638751d636a0564c0ad90da
Svastits <49677296+Svastits@users.noreply.github.com>
2026-07-15 15:27:53 +0200

    Rebase from 'upstream'

--- kuka_sunrise_fri_driver/test/test_driver_activation.py
+++ kuka_sunrise_fri_driver/test/test_driver_activation.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Aron Svastits
+# Copyright 2024 KUKA Hungaria Kft.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ def generate_test_description():
                 }.items(),
             ),
             launch.actions.TimerAction(
-                period=10.0,
+                period=20.0,
                 actions=[
                     launch.actions.ExecuteProcess(
                         cmd=["ros2", "lifecycle", "set", "robot_manager", "configure"],
@@ -55,7 +55,7 @@ def generate_test_description():
                 ],
             ),
             launch.actions.TimerAction(
-                period=15.0,
+                period=25.0,
                 actions=[
                     launch.actions.ExecuteProcess(
                         cmd=["ros2", "lifecycle", "set", "robot_manager", "activate"],
@@ -81,8 +81,8 @@ class TestDriverActivation(unittest.TestCase):
         )
         # Check for successful configuration and activation
         proc_output.assertWaitFor(
-            "Successful 'configure' of hardware 'lbr_iiwa14_r820'", timeout=15
+            "Successful 'configure' of hardware 'lbr_iiwa14_r820'", timeout=25
         )
         proc_output.assertWaitFor(
-            "Successful 'activate' of hardware 'lbr_iiwa14_r820'", timeout=20
+            "Successful 'activate' of hardware 'lbr_iiwa14_r820'", timeout=30
         )
